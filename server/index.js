12acdb503b20482e1a1888433654848b8625b548
Susy(Qinqin) Yang <susyyang24@gmail.com>
2021-04-15 15:54:05 -0600

    all people data is retrived

--- server/index.js
+++ server/index.js
@@ -1,11 +1,42 @@
 const app = require('express')();
 const path = require('path');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 app.use(bodyParser.json());
 
-app.get('get', (req, res) => {
-    res.status(201).json('the server responded')
+/* ===> allPeople section: data query and data cleaning up <===== */
+let nextPeoplePageUrl = 'http://swapi.dev/api/people/?page=1';
+let allPeople = [];
+
+const retrievePeopleOnePage = async() => {
+    try {
+        const peopleOnePage = await axios.get(nextPeoplePageUrl);
+        let {next, results} = peopleOnePage.data
+        nextPeoplePageUrl = next;
+        allPeople = allPeople.concat(results)
+        // console.log('allPeople', allPeople, 'nextPeoplePageUrl', nextPeoplePageUrl)
+    }
+    catch(err) {
+        console.log('swapi.dev: request people error:', err)
+    }
+};
+
+const sortAllPeople = (allPeople) => {
+
+}
+
+app.get('/people', async (req, res) => {
+    await retrievePeopleOnePage();
+    while (typeof nextPeoplePageUrl  === 'string') {
+        await retrievePeopleOnePage();
+    }
+
+    if (allPeople.length > 0) {
+        res.status(200).json(allPeople);
+    } else {
+        res.status(404).json('Failed to find people list');
+    }
 })
 
 app.listen(3000, ()=> console.log('Server is listening on port 3000'))
