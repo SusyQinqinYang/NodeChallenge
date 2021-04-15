e2038f2ee462e09ebfd9a1cc325411a45455a159
Susy(Qinqin) Yang <susyyang24@gmail.com>
2021-04-15 17:42:57 -0600

    completed the planets api

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const app = require('express')();
 const path = require('path');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const Promise = require("bluebird");
 
 app.use(bodyParser.json());
 
-/* ===> allPeople section: data query and data cleaning up <===== */
+/* ===> allPeople API section: data retrieve and sort <===== */
 let nextPeoplePageUrl = 'http://swapi.dev/api/people/?page=1';
 let allPeople = [];
 
@@ -15,7 +16,6 @@ const retrievePeopleOnePage = async() => {
         let {next, results} = peopleOnePage.data
         nextPeoplePageUrl = next;
         allPeople = allPeople.concat(results)
-        // console.log('allPeople', allPeople, 'nextPeoplePageUrl', nextPeoplePageUrl)
     }
     catch(err) {
         console.log('swapi.dev: request people error:', err)
@@ -34,9 +34,61 @@ app.get('/people/', async (req, res) => {
     if (allPeople.length > 0) {
         res.status(200).json(allPeople);
     } else {
-        res.status(404).json('Failed to find people list');
+        res.status(404).json('Failed to retrieve people list');
     }
 })
+
+/* ===> allPlanets API section: data retrieve and modification <===== */
+let nextPlanetsPageUrl = 'http://swapi.dev/api/planets?page=1';
+let allPlanets = [];
+
+const retrievePlanetsOnePage = async() => {
+    try {
+        const planetsOnePage = await axios.get(nextPlanetsPageUrl);
+        let {next, results} = planetsOnePage.data
+        nextPlanetsPageUrl = next;
+        allPlanets= allPlanets.concat(results)
+    }
+    catch(err) {
+        console.log('swapi.dev: request planets error:', err)
+    }
+};
+
+const fetchAllResidentsName = async (residentsUrlArr) => {
+    const allNamePromise = residentsUrlArr.map(eachResidentsUrl => axios.get(eachResidentsUrl));
+  
+    return axios.all(allNamePromise).then(
+      axios.spread((...responses) => {
+        return responses.map(allInfoOfAResident => allInfoOfAResident.data.name)
+      }),
+    );
+};
+
+app.get('/planets/', async (req, res) => {
+    await retrievePlanetsOnePage();
+    while (typeof nextPlanetsPageUrl  === 'string') {
+        await retrievePlanetsOnePage();
+    }
+
+    if (allPlanets.length > 0) {
+        const modifyPlanets = (allPlanets) => {
+            return Promise.all(
+                allPlanets.map( async (planetInfo) => {
+                    planetInfo.residents = await fetchAllResidentsName(planetInfo.residents);
+                    return planetInfo;
+                })
+            )
+        }
+        
+        modifyPlanets(allPlanets)
+        .then((modifiedPlanets) => {
+            res.status(200).json(modifiedPlanets);
+        })
+    } else {
+        res.status(404).json('Failed to retrieve the planets list');
+    }
+})
+
 
 app.listen(3000, ()=> console.log('Server is listening on port 3000'))
 
